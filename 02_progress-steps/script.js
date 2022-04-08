0969b1853b74dcafe1f4e8ca17c5a8409d19a833
felixwurst <felix.wurst@gmail.com>
2022-04-08 13:26:19 +0200

    second project completed

---
+++ 02_progress-steps/script.js
@@ -0,0 +1,44 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+  currentActive++;
+  if (currentActive > circles.length) currentActive = circles.length;
+  update();
+});
+
+prev.addEventListener('click', () => {
+  currentActive--;
+  if (currentActive < 1) currentActive = 1;
+  update();
+});
+
+function update() {
+  // adds or removes the active class to the circles
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  // calculates the length of the progress bar
+  const actives = document.querySelectorAll('.active');
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+  // enables or disables the prev and next buttons
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
