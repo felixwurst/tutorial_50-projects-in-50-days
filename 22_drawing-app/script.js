3a33887fd88c50939ad03baa17b56c01d3e2c8f4
felixwurst <felix.wurst@gmail.com>
2022-07-06 19:22:07 +0200

    mouse events & drawing

--- 22_drawing-app/script.js
+++ 22_drawing-app/script.js
@@ -2,8 +2,37 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
+let isPressed = false;
 let color = 'black';
 let x, y;
+
+canvas.addEventListener('mousedown', e => {
+  isPressed = true;
+
+  // returns the coordinates of the mouse pointer, relative to the target element
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', () => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener('mousemove', e => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -17,9 +46,6 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
-
-drawCircle(100, 200);
-drawLine(300, 300, 300, 500);
