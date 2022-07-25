fd16bac5ce7f6cc406befd87989394f684198288
felixwurst <felix.wurst@gmail.com>
2022-07-25 19:41:09 +0200

    dynamic profile cards

--- 28_github-profiles/script.js
+++ 28_github-profiles/script.js
@@ -2,18 +2,7 @@ const APIURL = 'https://api.github.com/users/';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-
-// getUser('bradtraversy');
-
-async function getUser(username) {
-  try {
-    const {data} = await axios(APIURL + username);
-
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const main = document.getElementById('main');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -25,3 +14,51 @@ form.addEventListener('submit', e => {
     search.value = '';
   }
 });
+
+async function getUser(username) {
+  try {
+    const {data} = await axios(APIURL + username);
+
+    createUserCard(data);
+  } catch (error) {
+    if (error.response.status == 404) {
+      createErrorCard('No profile with this username');
+    }
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+    <div class="card">
+      <div>
+        <img
+          src="${user.avatar_url}"
+          alt="${user.name}"
+          class="avatar"
+        />
+      </div>
+      <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+          <li>${user.followers} <strong>Followers</strong></li>
+          <li>${user.following} <strong>Following</strong></li>
+          <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+        <div id="repos"></div>
+      </div>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
